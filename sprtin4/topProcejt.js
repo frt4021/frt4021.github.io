ff73fe77c90958f00e7633fbc8667a9e578489df
Firat Bilgen <100495507+frt4021@users.noreply.github.com>
2024-05-27 12:04:49 +0300

    Update topProcejt.js

--- sprtin4/topProcejt.js
+++ sprtin4/topProcejt.js
@@ -2,18 +2,11 @@ document.addEventListener('DOMContentLoaded', fetchNews);
 
 function fetchNews() {
     const apiKey = '630916754d7b48eeb673d0ab22092b15';
-    const url = `https://newsapi.org/v2/top-headlines?category=technology&language=en&apiKey=${apiKey}`;
-
-    // Optionally use a CORS proxy
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    const fullUrl = proxyUrl + url;
+    const originalUrl = 'https://newsapi.org/v2/top-headlines?category=technology&language=en&apiKey=' + apiKey;
+    const url = 'https://corsproxy.io/?' + encodeURIComponent(originalUrl);
 
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', fullUrl, true); // Use fullUrl if using the CORS proxy
-
-    // Add the required headers
-   // xhr.setRequestHeader('origin', 'https://frt4021.github.io'); // replace with your domain
-    //xhr.setRequestHeader('x-requested-with', 'XMLHttpRequest');
+    xhr.open('GET', url, true);
 
     xhr.onload = function() {
         if (this.status === 200) {
@@ -22,7 +15,7 @@ function fetchNews() {
             let output = '';
 
             articles.forEach(article => {
-                const imageUrl = article.urlToImage ? article.urlToImage : 'placeholder.jpg'; // Eğer resim yoksa yerine bir placeholder koyabilirsiniz
+                const imageUrl = article.urlToImage ? article.urlToImage : 'placeholder.jpg';
 
                 output += `
                     <div class="news-item">
@@ -41,8 +34,10 @@ function fetchNews() {
             console.error('Error occurred!');
         }
     };
+
     xhr.onerror = function() {
         console.error('Request error!');
     };
+
     xhr.send();
 }
